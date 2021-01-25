726059d4f64ec54841f874e50812c1666c4465f3
thibautvangoethem <thibaut.vangoethem@student.uantwerpen.be>
2021-01-25 17:26:13 +0100

    Graph creation

--- similarityGraph.py
+++ similarityGraph.py
@@ -1,5 +1,30 @@
+import pickle
+import matplotlib.pyplot as plt
+import math
+
+def getData():
+    with open('obj/similarity.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
 def main():
-    pass
+    data=getData()
+    counted_vals=[0]*10
+    names=["0-10","10-20","20-30","30-40","40-50","50-60","60-70","70-80","80-90","90-100"]
+    for datapoint in data:
+        val=data[datapoint]
+        index=math.floor(val*10)
+        counted_vals[index]+=1
+
+    fig, ax = plt.subplots()
+    for idx,i in enumerate(counted_vals):
+        b = ax.bar(names[idx], i, 1)
+
+    ax.set_yscale('log')
+    ax.set_xlabel('similarity between documents (%)')
+    ax.set_ylabel('Number of documents')
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
