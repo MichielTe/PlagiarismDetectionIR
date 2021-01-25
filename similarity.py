f9bbd2119a5ef37748dfc8404a2a1ba4aa0b9342
thibautvangoethem <thibaut.vangoethem@student.uantwerpen.be>
2021-01-25 17:03:36 +0100

    jaccard similarity calculation

---
+++ similarity.py
@@ -0,0 +1,24 @@
+import pickle
+
+def getData():
+    with open('obj/shingles.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+def main():
+    pairwise_similarity_map=dict()
+    data = getData()
+    for outer_doc_counter in range(len(data)):
+        outer_set=data[outer_doc_counter]
+        for inner_doc_counter in range(outer_doc_counter+1,len(data)):
+            inner_set = data[inner_doc_counter]
+            union=outer_set.union(inner_set)
+            intersect=outer_set.intersection(inner_set)
+            jaccard_sim=len(intersect)/len(union)
+            pairwise_similarity_map[(outer_doc_counter,inner_doc_counter)]=jaccard_sim
+    with open('obj/similarity.pkl', 'wb') as file:
+        pickle.dump(pairwise_similarity_map, file)
+    return 0
+
+if __name__ == "__main__":
+    main()
